1e8d6772a9974775502a35bf853a762647a8f79c
Michelle Bello <michebeencalada@gmail.com>
2025-03-18 17:10:13 -0600

    Create índice.jsx

---
+++ origen/índice.jsx
@@ -0,0 +1,5 @@
+importar Aplicación de "./Aplicación";
+importar Reaccionar de "reaccionar";
+importar ReatDOM de "reacción-dom";
+
+ReatDOM.prestar(<Aplicación />, documento.obtenerElementoPorId("raiz"));
